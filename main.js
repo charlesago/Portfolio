38397bd723ddeef399a41bf62e9968584f28c6b5
charlesagostinelli <chacha.agostinelli@gmail.com>
2023-01-09 07:36:14 +0100

    parallax

--- main.js
+++ main.js
@@ -1,21 +1,6 @@
+NavBar = document.querySelector(".nav")
 
-const text = document.querySelector("#text")
-const stars = document.querySelector("#stars")
-const monn = document.querySelector("#moon")
-const montainefront = document.querySelector("#moutainfront")
-const montainbehind = document.querySelector("#moutainbehind")
-const btn = document.querySelector('#btn')
-window.addEventListener('scroll', ()=>{
-    let value = window.scrollY;
+NavBar.addEventListener('scroll', ()=>{
 
-   stars.style.left = value * 0.25 + 'px';
-    monn.style.top = value * 1.05 + 'px';
-    montainbehind.style.top = value * 0.5 + 'px';
-    montainefront.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4 + 'px';
-    text.style.marginTop = value * 1.5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
+    NavBar.style.backgroundColor = 'blue'
 })
-
-
-
